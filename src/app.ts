1c2c9e13a2505b050652c7abd830ebf31d19f334
md-galib-hossain <mdgalib23@gmail.com>
2024-03-30 02:02:49 +0600

    update

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req: Request, res: Response) => {
   res.send({
-    message: "Hello",
+    message: "Hello dev",
   });
 });
 
